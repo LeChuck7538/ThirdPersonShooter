70d9080b903944eefa26360032b4bd6a88e99894
PaulB <PaulBerry7538@Gmail.com>
2026-09-14 22:13:16 +0100

    Added Animation Blueprint

---
+++ Source/ThirdPersonShooter/Private/Characters/Player/PlayerAnimInstance.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ThirdPersonShooter/Public/Characters/Player/PlayerAnimInstance.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "ThirdPersonShooter/Public/Characters/Player/ShooterCharacter.h"
+
+void UPlayerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	if (APawn* PlayerPawn = TryGetPawnOwner())
+	{
+		ShooterCharacter = Cast<AShooterCharacter>(PlayerPawn);
+		MovementComponent = ShooterCharacter ? ShooterCharacter->GetCharacterMovement() : nullptr;
+	}
+}
+
+void UPlayerAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	// Check each frame if we have a valid reference to the ShooterCharacter.
+	APawn* OwningPawn = TryGetPawnOwner();
+	if (OwningPawn != ShooterCharacter || !IsValid(MovementComponent))
+	{
+		ShooterCharacter = Cast<AShooterCharacter>(OwningPawn);
+		MovementComponent = ShooterCharacter ? ShooterCharacter->GetCharacterMovement() : nullptr;
+	}
+	
+	if (!ShooterCharacter || !MovementComponent) return;
+	
+	const FVector Velocity = ShooterCharacter->GetVelocity();
+	Speed = Velocity.Size2D();
+	
+	bIsInAir = MovementComponent->IsFalling();
+	
+	// Is the player actively providing movement input?
+	bIsAccelerating = MovementComponent->GetCurrentAcceleration().SizeSquared() > KINDA_SMALL_NUMBER;
+}
+
+
+
+
---
+++ Source/ThirdPersonShooter/Public/Characters/Player/PlayerAnimInstance.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAnimInstance.generated.h"
+
+class UCharacterMovementComponent;
+class AShooterCharacter;
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONSHOOTER_API UPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+	virtual void NativeInitializeAnimation() override;
+	
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+	
+private:
+	UPROPERTY(Transient, VisibleAnywhere, BlueprintReadOnly, Category = "Player", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<AShooterCharacter> ShooterCharacter = nullptr;
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UCharacterMovementComponent> MovementComponent = nullptr;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player", meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir = false;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player", meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating = false;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player", meta = (AllowPrivateAccess = "true"))
+	float Speed = 0.0f;
+	
+};
